8dff360632fee1843c4369b11cfca3a5d84b7fd2
Eric Wang <wang.eric1115@gmail.com>
2022-03-26 12:12:47 -0700

    added dashboard stuff

--- HealthWealth/urls.py
+++ HealthWealth/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from numpy import sign
 
 from landing.views import landing
 from login.views import login
 from signup.views import signup
+from dashboard.views import dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', landing, name='landing'),
     path('login', login, name='login'),
     path('signup', signup,  name='signup'),
+    path('dashboard', dashboard, name='dashboard'),
 ]
