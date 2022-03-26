46b1dd54b69ed3d52a142082859699717f40fea6
Jonathan Lo <jonathanlo411@gmail.com>
2022-03-26 13:42:03 -0700

    login/signup finished

--- HealthWealth/urls.py
+++ HealthWealth/urls.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from django.urls import path
 
 from landing.views import landing
-from login.views import login
+from login.views import login_pg
 from signup.views import signup
 from dashboard.views import dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', landing, name='landing'),
-    path('login', login, name='login'),
+    path('login', login_pg, name='login'),
     path('signup', signup,  name='signup'),
     path('dashboard', dashboard, name='dashboard'),
 ]
