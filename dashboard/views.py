0a0fe5c860d552074826c76fbb9e9cf859d5b146
Eric Wang <wang.eric1115@gmail.com>
2022-03-27 03:42:24 -0700

    added dataset
    added dataset

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,12 +3,85 @@ from django.http import JsonResponse
 from django.http import HttpResponseRedirect
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+import numpy as np
+
+import pandas as pd
 
 # Models
 from signup.models import UserProfile
 
+# Helper Functions
+
+
+def calculate_percentile(age, sex, value):
+    if sex == 'female':
+        if age == 13:
+            mean, stderr = 125.8, 3.72
+        if age == 14:
+            mean, stderr = 137.0, 2.19
+        if age == 15:
+            mean, stderr = 137.5, 3.36
+        if age == 16:
+            mean, stderr = 144.9, 3.57
+        if age == 17:
+            mean, stderr = 149.7, 3.57
+        if age == 18:
+            mean, stderr = 151.6, 3.70
+        if age == 19:
+            mean, stderr = 156.5, 4.37
+        if age in range(20, 30):
+            mean, stderr = 165.0, 2.49
+        if age in range(30, 40):
+            mean, stderr = 174.9, 2.19
+        if age in range(40, 50):
+            mean, stderr = 178.1, 3.14
+        if age in range(50, 60):
+            mean, stderr = 173.5, 2.23
+        if age in range(60, 70):
+            mean, stderr = 173.5, 2.23
+        if age in range(60, 70):
+            mean, stderr = 172.4, 2.68
+        if age in range(70, 80):
+            mean, stderr = 164.6, 2.10
+        if age > 80:
+            mean, stderr = 149.7, 2.22
+    elif sex == 'male':
+        if age == 13:
+            mean, stderr = 133.2, 3.33
+        if age == 14:
+            mean, stderr = 143.6, 3.88
+        if age == 15:
+            mean, stderr = 160.2, 4.25
+        if age == 16:
+            mean, stderr = 157.2, 3.37
+        if age == 17:
+            mean, stderr = 170.0, 4.16
+        if age == 18:
+            mean, stderr = 166.7, 4.34
+        if age == 19:
+            mean, stderr = 176.2, 3.59
+        if age in range(20, 30):
+            mean, stderr = 188.6, 3.17
+        if age in range(30, 40):
+            mean, stderr = 208.1, 2.68
+        if age in range(40, 50):
+            mean, stderr = 206.9, 2.29
+        if age in range(50, 60):
+            mean, stderr = 202.5, 2.48
+        if age in range(60, 70):
+            mean, stderr = 201.2, 1.80
+        if age in range(60, 70):
+            mean, stderr = 193.4, 2.39
+        if age in range(70, 80):
+            mean, stderr = 80.5, 0.90
+        if age > 80:
+            mean, stderr = 177.5, 1.98
+    sim_distr = np.random.normal(20, 2.5, 10000)
+    return np.percentile(sim_distr, value)
 
 # Render Requests
+
+
 @login_required(login_url='/login/')
 def dashboard(request):
     user = UserProfile.objects.get(username=request.user)
@@ -22,6 +95,7 @@ def dashboard(request):
     }
     return render(request, 'dashboard/dashboard.html', data)
 
+
 @login_required(login_url='/login/')
 def resources(request):
     user = UserProfile.objects.get(username=request.user)
@@ -32,8 +106,11 @@ def resources(request):
         'age': user.age,
         'excercise': user.excercise,
         'diet': user.diet,
+        'percentile': calculate_percentile(user.age, user.sex, user.weight)
     }
+
     return render(request, 'dashboard/resources.html', data)
+
 
 @login_required(login_url='/login/')
 def settings(request):
@@ -47,6 +124,7 @@ def settings(request):
         'diet': user.diet,
     }
     return render(request, 'dashboard/settings.html', data)
+
 
 @login_required(login_url='/login/')
 def about(request):
@@ -62,6 +140,8 @@ def about(request):
     return render(request, 'dashboard/about.html', data)
 
 # API Endpoint
+
+
 def get_weight(request):
     if request.method == 'GET':
         weight = [200, 201, 200.5, 199, 200]
@@ -72,6 +152,7 @@ def get_weight(request):
         }
         return JsonResponse(response, status=200)
     return JsonResponse({"error": "Something went wrong"}, status=400)
+
 
 def log_out(request):
     logout(request)
